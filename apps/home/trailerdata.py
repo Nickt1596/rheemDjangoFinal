ecc02983ec44e7b59bd47290c8a0c3c79cfa58c5
Nick Tallarico <Ntgw1596@gmail.com>
2021-11-15 17:57:46 -0500

    Implement Selenium

--- apps/home/trailerdata.py
+++ apps/home/trailerdata.py
@@ -100,11 +100,11 @@ def get_options():
 
 def get_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     return driver
     # chrome_options = webdriver.ChromeOptions()
     # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
