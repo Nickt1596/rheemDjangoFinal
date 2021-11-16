7297eff5ff8b7695ffce63f0c3c6cbee5d9a7797
Nick Tallarico <Ntgw1596@gmail.com>
2021-11-15 21:01:27 -0500

    Fixed trailer-locations

--- apps/home/trailerdata.py
+++ apps/home/trailerdata.py
@@ -213,11 +213,11 @@ def run():
     #     executor.submit(skybitztesting2, trailerDict, driver=get_driver())
     #     executor.submit(xtralease, trailerDict, driver=get_driver())
     #     executor.submit(spireon, trailerDict, driver=get_driver())
-    skybitztesting(trailerDict, get_driver())
-    skybitztesting2(trailerDict, get_driver())
     xtralease(trailerDict, get_driver())
     spireon(trailerDict, get_driver())
-    time.sleep(2)
+    skybitztesting(trailerDict, get_driver())
+    skybitztesting2(trailerDict, get_driver())
+    time.sleep(1)
     dict = trailerDict.get_dict()
     return dict
 
